7ea91b89fe814e73735cd872701889e2cc47cdf5
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-07-18 22:44:21 +0500

    middleware / Router/ Mounting the routers

--- TourTron/app.js
+++ TourTron/app.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express(); // Creating an instance of an Express application
 
+//Middlewares
+
 //This middleware ensures that incoming request bodies are parsed as JSON.
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('hello from the middlware ');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use(morgan('dev'));
 // Reading and parsing tour data from a JSON file
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 
+//Tours Route Handlers
 const getAllTours = (req, res) => {
   // api version - Ensures clients using older versions of the API won't break when new versions are released, and allows changes in v2 without breaking v1 users.
   res.status(200).json({
     status: 'success',
     results: tours.length,
+    requestedAt: req.requestTime,
     data: {
       tours,
     },
@@ -23,7 +40,7 @@ const getTour = (req, res) => {
   const tour = tours.find((e) => e.id === id);
 
   if (!tours) {
-    return res.status(404).json({
+    res.status(404).json({
       status: 'fail',
       message: 'Inavlid Id',
     });
@@ -99,25 +116,54 @@ const deleteTour = (req, res) => {
   });
 };
 
-// // GET request for all tours
-// app.get('/api/v1/tours', getAllTours);
-// //get request responding to the url parmeters
-// app.get('/api/v1/tours/:id', getTour);
-// //post request
-// app.post('/api/v1/tours', createTour);
-// //patch request
-// app.patch('/api/v1/tours/:id', updateTour);
-// //delete request
-// app.delete('/api/v1/tours/:id', deleteTour);
+//Users Route Handlers
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implemented',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implemented',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implemented',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implemented',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implemented',
+  });
+};
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+// Creating the Express Router instances for tours and users
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+// tours routes
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+// users routes
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
+//Mounting the Routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+//start server`
 const port = 3000; // Setting the port number for the server to listen on
 // Starting the server and listening for incoming requests on the specified port
 app.listen(port, () => {
