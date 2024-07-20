61def20159120ec9af0bd63e2977f60205c0402b
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-07-21 01:53:14 +0500

    MogoDB connection

--- TourTron/server.js
+++ TourTron/server.js
@@ -1,10 +1,65 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT || 3000;
 const app = require('./app');
 
-// console.log(process.env);
+const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME } =
+  process.env;
+
+const connectionString = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?retryWrites=true&w=majority`;
+
+// Added the connection string and options to mongoose.connect
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    // console.log(con.connection);
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB Atlas:', err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    maxlength: 50,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const tour = new Tour({
+  name: 'The park camper',
+
+  price: 399,
+});
+
+tour
+  .save()
+  .then((doc) => {
+    console.log('New tour saved to DB:', doc);
+  })
+  .catch((err) => {
+    console.error('Error saving tour to DB:', err);
+  });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
