793d7ba24c875b876dd4a13b3305a28e18eee269
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-07-22 23:08:28 +0500

    made importing json data script into the Mongodb

---
+++ TourTron/dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config({ path: './config.env' });
+
+const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME } =
+  process.env;
+
+const connectionString = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString);
+
+//read the json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+//Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data imported successfully');
+  } catch (e) {
+    console.error('Error importing data', e);
+  }
+  process.exit();
+};
+
+//Delete all data from DB
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted successfully');
+  } catch (e) {
+    console.error('Error deleting data', e);
+    process.exit();
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// console.log(process.argv);
