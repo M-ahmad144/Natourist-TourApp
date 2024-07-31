4e609e0d45e9af9da76a7f6340d1048ea10cbd67
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-07-31 21:52:06 +0500

    add HTTP headers for enhanced protection use some security middleware prevent from sql injection

--- TourTron/app.js
+++ TourTron/app.js
@@ -1,36 +1,62 @@
 const express = require('express');
 const AppErr = require('./utils/AppErr');
 const morgan = require('morgan');
-
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/usersRoutes');
 const globalErrorHandler = require('./controllers/ErrController');
+
 const app = express();
+
+// Serve static files
 app.use(express.static('./public'));
 
-//Middlewares
+//_________________________ Global Middlewares
 
-app.use(express.json());
+// Set security HTTP headers
+app.use(helmet());
+
+// Parse incoming JSON requests into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data Sanitization against XSS (malachous HTML code)
+app.use(xss());
+
+// Add request time to the request object
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-console.log(process.env.NODE_ENV);
+// Log requests to the console in development mode
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//Mounting the Routers
+// Rate Limiting Middleware to prevent abuse
+const limiter = rateLimit({
+  max: 100, // Limit each IP to 100 requests per windowMs
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter); // Apply to all routes starting with /api
+
+// Mounting Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-//handle udefined routes
+// Handle undefined routes
 app.all('*', (req, res, next) => {
-  next(new AppErr(`Can't find ${req.originalUrl} on this server`, 404)); //skips all remaining non-error-handling middleware and passes the error to the next error-handling middleware
+  next(new AppErr(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-//___ Global Error Handling Middleware ___
+// Global Error Handling Middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
