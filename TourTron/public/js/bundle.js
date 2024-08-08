972397d9487febd1504aad55529978f0a573de19
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-08-08 23:58:43 +0500

    add map to see locations of the tours

--- TourTron/public/js/bundle.js
+++ TourTron/public/js/bundle.js
@@ -64054,7 +64054,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57698" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59625" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
