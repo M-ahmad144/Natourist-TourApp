7aff5380c00995b2dc59103ef124e2d10dde93d0
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-08-12 01:25:01 +0500

    adding user images to database

--- Natourist/routes/usersRoutes.js
+++ Natourist/routes/usersRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
@@ -12,26 +13,26 @@ router.get('/logout', authController.logout);
 router.post('/forgetPassword', authController.forgetPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-// Protected routes middleware - protcts all routes after this are protected
+
 router.use(authMiddleware.protect);
 
-// Update the current user's password
+
 router.patch('/updateMyPassword', authController.updatePassword);
-// Get the current user's details
+
 router.get(
   '/me',
   userController.getMe, // Sets the userId in the params
   userController.getUser,
 );
-// Update the current user's details
+
 router.patch('/updateMe', userController.updateMe);
 // Deactivate the current user's account
 router.delete('/deleteMe', userController.deleteMe);
 
-//after this middleware, only admin can access these routes
+
 router.use(authMiddleware.restrictTo('admin'));
 
-// User management routes for admins
+
 router
   .route('/')
   .get(userController.getAllUsers)
