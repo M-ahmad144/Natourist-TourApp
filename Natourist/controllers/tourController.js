2665a983565fe052a058aacd5494c8a2c5b4075d
Muhammad Ahmad <muhammadahmadmughal0987@gmail.com>
2024-08-14 02:30:33 +0500

    Revert "forget password reset password funtionality with the sending email to client"
    This reverts commit a905f51957312150b605856526da5a3d50afeae5.

---
+++ Natourist/controllers/tourController.js
@@ -0,0 +1,239 @@
+const multer = require('multer');
+const sharp = require('sharp');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+const AppErr = require('../utils/AppErr');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  //req.files use for uploading multiple images
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`; //put the name of the image in the body because the updateTour function takes the entire req.body object
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = []; //put the name of the image in the body because the updateTour function takes the entire req.body object
+  await Promise.all(
+    //req.params.id is the tour id
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    }),
+  );
+
+  next();
+});
+
+// Middleware to alias the top 5 cheap tours
+exports.aliasToptour = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage price';
+  req.query.fields = 'name price ratingsAverage summary difficulty';
+  next();
+};
+
+// Get all tours
+exports.getAllTours = factory.getAllDocuments(Tour);
+
+// Get a single tour
+exports.getTour = factory.getDocument(Tour, { path: 'reviews' }); //path: 'reviews' is present in the Tour model virtual Schema
+
+// Create a new tour
+exports.createTour = factory.createDocument(Tour);
+
+// Update an existing tour
+exports.updateTour = factory.updateDocument(Tour);
+
+// Delete a tour
+exports.deleteTour = factory.deleteDocument(Tour);
+
+//get tour stats
+exports.getToursStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: {
+          $toUpper: '$difficulty',
+        },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
+      },
+    },
+    {
+      $sort: { avgPrice: 1 },
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats,
+    },
+  });
+});
+
+//get monthy plan
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1;
+  const plan = await Tour.aggregate([
+    {
+      // The $unwind stage deconstructs the startDates array field,
+      // This means that if a tour has multiple start dates, each date will have its own document.
+      $unwind: '$startDates',
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTours: { $sum: 1 },
+        tours: { $push: '$name' },
+      },
+    },
+    {
+      $addFields: { month: '$_id' },
+    },
+    {
+      //hide the _id field
+      $project: {
+        _id: 0,
+      },
+    },
+    {
+      $sort: { numTours: -1 },
+    },
+    {
+      $limit: 12,
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan,
+    },
+  });
+});
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+// get tours within a certain distance
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  // Validate lat and lng
+  if (!lat || !lng) {
+    return next(
+      new AppErr(
+        'Please provide valid latitude and longitude in the format lat,lng.',
+        400,
+      ),
+    );
+  }
+
+  // Convert distance to radians if unit is miles (3963.2 miles is Earth's radius and 6378.1 km is Earth's radius)
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  const tours = await Tour.find({
+    startLocation: {
+      $geoWithin: { $centerSphere: [[lng, lat], radius] },
+    },
+  });
+
+  // console.log(distance, lat, lng, unit);
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// /distances/:latlng/unit/:unit
+// get distance between two points
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  // Validate lat and lng
+  if (!lat || !lng) {
+    return next(
+      new AppErr(
+        'Please provide valid latitude and longitude in the format lat,lng.',
+        400,
+      ),
+    );
+  }
+  const distance = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: unit === 'mi' ? 0.000621371 : 0.001, //convert the distance to miles or km
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+
+    data: {
+      distance,
+    },
+  });
+});
