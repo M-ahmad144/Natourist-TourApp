1c7d187565d84782b80372145d7d1943f6edf4ae
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-08-14 00:39:52 +0500

    Revert "forget password reset password funtionality with the sending email to client"
    This reverts commit a905f51957312150b605856526da5a3d50afeae5.

---
+++ Natourist/controllers/reviewController.js
@@ -0,0 +1,24 @@
+const Review = require('../models/reviewModel');
+const factory = require('./handlerFactory');
+
+//middleware to set tourId and userId for the review
+exports.setTourUserId = (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id; //coming from the authController.protect
+  next();
+};
+
+//get all reviews
+exports.getAllReviews = factory.getAllDocuments(Review);
+
+//get a single review
+exports.getReview = factory.getDocument(Review);
+
+//create a review
+exports.createReview = factory.createDocument(Review);
+
+//delete a review
+exports.deleteReview = factory.deleteDocument(Review);
+
+//update a review
+exports.updateReview = factory.updateDocument(Review);
